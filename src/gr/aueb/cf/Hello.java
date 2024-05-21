a502d6c20b01bfe8b26e9d5e8e9c30d5e9574c2c
s1naf <theofaniskavvadias@gmail.com>
2024-05-21 19:40:03 +0300

    Hello coding Factory with many !

--- src/gr/aueb/cf/Hello.java
+++ src/gr/aueb/cf/Hello.java
@@ -2,6 +2,8 @@ package gr.aueb.cf;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World coding Factory!!!!!!");
+
+
     }
 }
